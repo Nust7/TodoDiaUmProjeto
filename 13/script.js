08129fc7a7b14f1e4a2774fc689fe3db1731ea65
Nust7 <97990318+Nust7@users.noreply.github.com>
2024-10-15 19:38:53 -0300

    Adicionando Mais um projeto html,CSS e Js

---
+++ 13/script.js
@@ -0,0 +1,14 @@
+document.getElementById('button-ask').addEventListener('click', function() {
+    const resposta = document.querySelector('.ask-1');
+    resposta.classList.toggle('show'); // Alterna a classe 'show'
+});
+
+document.getElementById('button-ask2').addEventListener('click', function() {
+    const resposta2 = document.querySelector('.ask-2');
+    resposta2.classList.toggle('show'); // Alterna a classe 'show'
+});
+
+document.getElementById('button-ask3').addEventListener('click', function() {
+    const resposta3 = document.querySelector('.ask-3');
+    resposta3.classList.toggle('show'); // Alterna a classe 'show'
+});
